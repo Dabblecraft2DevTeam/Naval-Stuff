6c60b8272916fdc997b04da6f1526ff2a675a45c
BuildTools <unconfigured@null.spigotmc.org>
2017-03-06 17:47:50 -0400

    fix
    this is a revert to fix maven issues this plugin will be redone for
    maven

--- com/maximuspayne/navycraft/Periscope.java
+++ com/maximuspayne/navycraft/Periscope.java
@@ -1,18 +1,24 @@
-package com.maximuspayne.navycraft;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
+/*    */ package com.maximuspayne.navycraft;
+/*    */ 
+/*    */ import org.bukkit.Location;
+/*    */ 
+/*    */ public class Periscope
+/*    */ {
+/*    */   public Location signLoc;
+/*    */   public Location scopeLoc;
+/*    */   public int periscopeID;
+/* 10 */   public boolean raised = true;
+/* 11 */   public boolean destroyed = false;
+/*    */   public org.bukkit.entity.Player user;
+/*    */   
+/*    */   public Periscope(Location signLocIn, int idIn) {
+/* 15 */     this.signLoc = signLocIn;
+/* 16 */     this.periscopeID = idIn;
+/*    */   }
+/*    */ }
 
 
-public class Periscope {
-	public Location signLoc;
-	public Location scopeLoc;
-	public int periscopeID;
-	public boolean raised=true;
-	public boolean destroyed=false;
-	public Player user;
-	public Periscope(Location signLocIn, int idIn)
-	{
-		signLoc = signLocIn;
-		periscopeID = idIn;
-	}
-}
+/* Location:              C:\Users\keough99\Desktop\jd-gui-windows-1.4.0\Navalstuff.jar!\com\maximuspayne\navycraft\Periscope.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */
